9eb1f2d0cb2c1a3a400c84aba120a2e54c5f3b78
Maggiemajiii <magzcw@bu.edu>
2023-01-29 15:44:27 -0500

    solve 2.1.si

--- HWs/hw1/PinCracker.py
+++ HWs/hw1/PinCracker.py
@@ -12,19 +12,42 @@ def generate_number():
 def one_sim_s1():
     key = '2022'
     count = 0
-    attempt = ""
+    attempt = ''
     while(attempt != key):
         attempt = generate_number()
         count += 1
-        if(attempt == key):
-            print("success\n")
+    return count
+
+def one_sim_s2():
+    key = '2022'
+    count = 0
+    attempt = ''
+    record = []
+    while(attempt != key):
+        attempt = generate_number()
+        if(attempt in record):
+            continue
+        else:
+            count +=1
+            record.append(attempt)
     return count
 
 
-def sim(nsim):
+def sim1(nsim):
     trials_record = []
     for i in range(nsim):
         trials = one_sim_s1()
+        trials_record.append(trials)
+        i +=1
+    sd = statistics.stdev(trials_record)
+    ave = statistics.mean(trials_record)
+    print(sd, ave, '\n')
+    return ave, sd
+
+def sim2(nsim):
+    trials_record = []
+    for i in range(nsim):
+        trials = one_sim_s2()
         trials_record.append(trials)
         i +=1
     sd = statistics.stdev(trials_record)
@@ -36,19 +59,24 @@ nsims = [i for i in range(100, 1001, 100)]
 print(nsims)
 ave_s1 = []
 sd_s1 = []
+ave_s2 = []
+sd_s2 = []
+
 for j in nsims:
-    cave, csd = sim(j)
-    ave_s1.append(cave)
-    sd_s1.append(csd)
+    cave1, csd1 = sim1(j)
+    cave2, csd2 = sim2(j)
+    ave_s1.append(cave1)
+    sd_s1.append(csd1)
+    ave_s2.append(cave2)
+    sd_s2.append(csd2)
 
 
 plt.errorbar(nsims, ave_s1, yerr=sd_s1, fmt='o', color='red', ecolor='black', capsize=5)
 plt.xlabel('Number of Simulations')
 plt.ylabel('Average Number of Trials')
 plt.title('Plot s1 with Error Bars')
+plt.savefig('s1plot.jpeg')
 plt.show()
-file_path = '/Users/maggie-z/Github/BUCS365/HWs/hw1/bar_plot_with_error_bars_s1.png'
-plt.savefig(file_path)
 
 
 
